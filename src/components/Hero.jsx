e970bfc4aab96b083f55cba5d7d734d31cb88a97
intellect-sam <alukoolakunlesamuel@gmail.com>
2023-06-26 03:42:22 +0100

    Navbar done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,31 +8,33 @@ const Hero = () => {
 
 
   return (
-    <section className="flex flex-col-reverse justify-center gap-5 py-10 mx-5 md:flex-row md:mx-20 md:flex md:py-80 md:px-80" >
+    <section className="flex flex-col-reverse justify-center px-10 py-4 md:gap-5 md:flex-row md:mx-40 md:flex md:py-80 md:px-96" >
   
-      <div className="w-full md:w-1/2">
-        <div className="text-4xl font-bold md:text-6xl font-pop">
+      <div className="w-full mt-[-250px] md:mt-0 justify-center text-center md:text-left">
+        <div className="w-full text-4xl font-bold md:text-6xl font-pop">
           Front-End React Developer 👋
         </div>
         <p className="py-4 text-sm font-light leading-loose text-justify md:w-3/4">
-          Hi, I'm Aluko Samuel. I'm a skilled frontend developer with a passion for creating captivating and user-friendly web experiences. 
+          Hi, I'm Aluko Samuel, a skilled frontend developer with a passion for creating captivating and user-friendly web experiences. 
           With expertise in HTML, CSS, and JavaScript, 
           I strive to bring designs to life and ensure seamless functionality across different platforms and devices 
         </p>
-        <div className="flex gap-5 text-2xl font-bold">
+        <div className="flex justify-center gap-5 text-2xl font-bold md:items-start">
           <BsGithub />
           <BsTwitter />
           <BsLinkedin/>
         </div>
       </div>
       
-      <div className="relative top-0 z-0 w-96"> 
-          <div>
-            <img src={back} alt="" />
-          </div>
-          <div className="relative z-auto bg-white border-4 border-black-500 rounded-full shadow-xl right-[-20px] top-[-270px] w-80 h-80">
-            <img src={Sam} alt="" className="w-screen h-full rounded-full" />
-          </div>
+      <div className="flex justify-center w-full">
+        <div className="relative top-0 w-96"> 
+            <div className="invisible md:visible">
+              <img src={back} alt="" />
+            </div>
+            <div className="relative z-0 bg-white border-4 border-black-500 rounded-full shadow-xl right-[-20px] top-[-270px] w-80 h-80">
+              <img src={Sam} alt="" className="w-screen h-full rounded-full" />
+            </div>
+        </div>
       </div>
     </section>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-
-import { close, menu } from "../assets";
+import { FaBars, FaTimes} from 'react-icons/Fa'
 import { navLinks } from "../constants";
 
 const Navbar = () => {
@@ -8,8 +7,8 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="flex justify-between py-4 md:items-center md:mt-6 md:py-6 md:mx-80 md:px-14">
-      <a href="/" className="font-bold font-pop">Sam.Dev</a>
+    <nav className="flex justify-between px-10 py-6 md:items-center md:mt-6 md:py-6 md:mx-80 md:px-14">
+      <a href="/" className="text-2xl font-bold font-pop">Sam.Dev</a>
 
       <div className="flex justify-between md:gap-8">
           <ul className="items-center justify-end flex-1 hidden list-none sm:flex">
@@ -26,7 +25,7 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-          <div className="justify-end">
+          <div className="justify-end invisible md:visible">
 
               <button className="justify-end px-2 py-1 text-sm font-bold text-white bg-red-700 rounded shadow-md hover:bg-blue-700">
                 Resume
@@ -37,19 +36,26 @@ const Navbar = () => {
 
 
       <div className="flex items-center justify-end flex-1 sm:hidden">
-        <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain text-black"
+        <div>
+        {toggle ? (
+        <FaTimes
+          className="object-contain w-8 h-8"
           onClick={() => setToggle(!toggle)}
         />
+      ) : (
+        <FaBars
+          className="object-contain w-8 h-8"
+          onClick={() => setToggle(!toggle)}
+        />
+      )}
+        </div>
 
         <div
           className={`${
             !toggle ? "hidden" : "flex-col gap-10"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 bg-white z-40 absolute top-20 right-0 left-0 w-full h-screen`}
         >
-          <ul className="flex flex-col items-start justify-end flex-1 list-none">
+          <ul className="flex flex-col items-center justify-center py-40 list-none">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
@@ -61,7 +67,7 @@ const Navbar = () => {
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
             ))}
-          <button className="justify-end px-2 py-2 text-sm font-light text-white bg-red-500 rounded shadow-md hover:bg-blue-700">
+          <button className="justify-end px-2 py-2 mt-10 text-sm font-light text-white bg-red-500 rounded shadow-md hover:bg-blue-700">
                 Download CV
               </button>
           </ul>
