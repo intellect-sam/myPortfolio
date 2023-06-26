f63929a900f1e19dcb183bf2455bcdf8b1f12543
intellect-sam <alukoolakunlesamuel@gmail.com>
2023-06-26 02:01:18 +0100

    Hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,38 @@
-import Sam from "../assets/images/sam.png";
-import { } from 'react-icons'
+import Sam from "../assets/images/sam.png"
+import back from "../assets/images/back.png"
+import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/Bs'
 
 
 
 const Hero = () => {
+
+
   return (
-    <section className="flex justify-center gap-5 mx-20 py-80 md:px-80" >
+    <section className="flex flex-col-reverse justify-center gap-5 py-10 mx-5 md:flex-row md:mx-20 md:flex md:py-80 md:px-80" >
   
-      <div className="w-1/2">
-        <div className="text-6xl font-bold font-pop">
+      <div className="w-full md:w-1/2">
+        <div className="text-4xl font-bold md:text-6xl font-pop">
           Front-End React Developer 👋
         </div>
-        <p className="w-3/4 py-4 text-sm font-light leading-loose text-justify">
+        <p className="py-4 text-sm font-light leading-loose text-justify md:w-3/4">
           Hi, I'm Aluko Samuel. I'm a skilled frontend developer with a passion for creating captivating and user-friendly web experiences. 
           With expertise in HTML, CSS, and JavaScript, 
           I strive to bring designs to life and ensure seamless functionality across different platforms and devices 
         </p>
+        <div className="flex gap-5 text-2xl font-bold">
+          <BsGithub />
+          <BsTwitter />
+          <BsLinkedin/>
+        </div>
       </div>
-      <div className="border border-red-500 rounded-full shadow-xl w-80 h-80">
-          <img src={Sam} alt="" className="w-full h-full rounded-md"/>
-        {/* <div className="bg-black border border-red-500 rounded-full border-s-4 w-70 h-70">
-
-        </div> */}
-
+      
+      <div className="relative top-0 z-0 w-96"> 
+          <div>
+            <img src={back} alt="" />
+          </div>
+          <div className="relative z-auto bg-white border-4 border-black-500 rounded-full shadow-xl right-[-20px] top-[-270px] w-80 h-80">
+            <img src={Sam} alt="" className="w-screen h-full rounded-full" />
+          </div>
       </div>
     </section>
   )
